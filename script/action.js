1a9a90f3f64c512a7c3f5294b1ccc6e4161f1f55
1215kkm <1215kkm@naver.com>
2024-09-12 17:10:58 +0900

    14기 강우진 섹션3

--- script/action.js
+++ script/action.js
@@ -7,3 +7,25 @@ $('#section1 .mobile li').mouseenter(function(){
         $('#section1 .tv .screen img').attr('src', 'images/screen'+liNum+'.png')
     }
 })
+
+$('#section1 .mobile').mouseenter(function(){
+    $('#section1 .robot').addClass('on')
+}).mouseleave(function(){
+    $('#section1 .robot').removeClass('on')
+})
+$('#section4 .monitors .pc').mouseenter(function(){
+    let pcH = $(this).height();
+    let imgH = $(this).find('img').height();
+    $(this).find('img').css('top', (pcH - imgH)+ 'px')
+}).mouseleave(function(){
+    $(this).find('img').css('top', 0)
+
+})
+$('#section4 .scr').mouseenter(function(){
+    let pcH = $(this).height();
+    let imgH = $(this).find('img').height();
+    $(this).find('img').css('top', pcH - imgH)
+}).mouseleave(function(){
+    $(this).find('img').css('top', 0)
+
+})
